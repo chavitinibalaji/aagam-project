0bd9f96580c5ca41f75c16e83dd36f5d6148a294
chavitinibalaji <balajichavitini@gmail.com>
2026-02-18 12:17:32 +0530

    Define CartItem model with associations

--- backend/models/cartItem.js
+++ backend/models/cartItem.js
@@ -1,1 +1,35 @@
+const { DataTypes } = require('sequelize');
 
+module.exports = (sequelize) => {
+  const CartItem = sequelize.define('CartItem', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    cartId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+      validate: { min: 1 },
+    },
+  }, {
+    tableName: 'cart_items',
+    timestamps: true,
+  });
+
+  CartItem.associate = (models) => {
+    CartItem.belongsTo(models.Cart, { foreignKey: 'cartId' });
+    CartItem.belongsTo(models.Product, { foreignKey: 'productId' });
+  };
+
+  return CartItem;
+};
